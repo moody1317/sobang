4af27b8ea7cddae440f367859003f5bd2f37a27a
moody1317 <soen13@chungbuk.ac.kr>
2026-06-22 22:14:19 +0900

    main.py 수정

--- app/main.py
+++ app/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+from app.api.v1 import admin, auth
+from app.core.database import Base, engine
+from app.models import station, user
+
+app = FastAPI(
+    title="Sobang Backend",
+    version="0.1.0",
+)
+
+@app.on_event("startup")
+def on_startup():
+    Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def root():
-    return {"message": "heelo fastAPI"}
+    return {"message": "Sobang Backend is running"}
+
+app.include_router(auth.router, prefix="/api/v1")
+app.include_router(adin.router, prefix="/api/v1")
